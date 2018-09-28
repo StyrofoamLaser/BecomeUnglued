3c53d006f1babc9d21ac9b3e964311d1bc17dad5
cody carlson <cody@carlsonhome.org>
2018-09-28 19:04:26 -0400

    adding some new env stuff, starting on new gun, added reactive target with events

---
+++ Become Unglued/Assets/Scripts/ProjectileTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class ProjectileTarget : MonoBehaviour {
+
+    public UnityEvent onHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.tag == "projectile")
+        {
+            onHit.Invoke();
+        }
+    }
+}
--- Become Unglued/Assets/Scripts/TouchpadMovement.cs
+++ Become Unglued/Assets/Scripts/TouchpadMovement.cs
@@ -8,6 +8,9 @@ public class TouchpadMovement : MonoBehaviour {
     [SerializeField]
     private Transform vrRig;
 
+    [SerializeField]
+    private Rigidbody body;
+
     private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
 
     private Hand hand;
@@ -20,6 +23,7 @@ public class TouchpadMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         hand = GetComponent<Hand>();
+        body = GetComponentInParent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -39,6 +43,9 @@ public class TouchpadMovement : MonoBehaviour {
                 var origY = vrRig.position.y;
                 vrRig.position += (transform.right * axis.x + transform.forward * axis.y) * Time.deltaTime * speed;
                 vrRig.position = new Vector3(vrRig.position.x, origY, vrRig.position.z);
+
+                //Vector3 newPos = body.position + (transform.right * axis.x + transform.forward * axis.y) * Time.deltaTime * speed;
+                //body.MovePosition(newPos);
             }
         }
 		
